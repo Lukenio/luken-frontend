6365eaa52a54eabc77ac055091e399959f6958d3
Andrey Ostashchenko <andreywizard99@gmail.com>
2018-02-26 18:16:50 +0200

    configure eslint & update folder structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import registerServiceWorker from 'setup/registerServiceWorker';
+
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
