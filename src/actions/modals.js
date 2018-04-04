0f51b5ca3e6fe273f3dfcc81bbff988bb49e6e88
Dmytro Lvivsky <dmytro.lvivsky@gmail.com>
2018-04-05 00:21:01 +0300

    Improved & fixed actions

--- src/actions/modals.js
+++ src/actions/modals.js
@@ -1,4 +1,11 @@
-import { SHOW_NEW_ACCOUNT_MODAL, HIDE_NEW_ACCOUNT_MODAL } from '../constants';
+import {
+  SHOW_NEW_ACCOUNT_MODAL,
+  HIDE_NEW_ACCOUNT_MODAL,
+  SHOW_WITHDRAW_REQUEST_MODAL,
+  HIDE_WITHDRAW_REQUEST_MODAL,
+  SHOW_ACCOUNT_ADDRESS_MODAL,
+  HIDE_ACCOUNT_ADDRESS_MODAL
+} from '../constants';
 
 export function showNewAccountModal() {
   return { type: SHOW_NEW_ACCOUNT_MODAL };
@@ -6,4 +13,20 @@ export function showNewAccountModal() {
 
 export function hideNewAccountModal() {
   return { type: HIDE_NEW_ACCOUNT_MODAL };
+}
+
+export function showWithdrawRequestModal() {
+  return { type: SHOW_WITHDRAW_REQUEST_MODAL };
+}
+
+export function hideWithdrawRequestModal() {
+  return { type: HIDE_WITHDRAW_REQUEST_MODAL };
+}
+
+export function showAccountAddressModal() {
+  return { type: SHOW_ACCOUNT_ADDRESS_MODAL };
+}
+
+export function hideAccountAddressModal() {
+  return { type: HIDE_ACCOUNT_ADDRESS_MODAL };
 }
