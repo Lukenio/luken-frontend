a9758bf43fd3272d2732b2ffe535740ede47be47
Ruslan Aletdinov <cgruslan@gmail.com>
2018-04-28 05:52:25 +0500

    Added user dropdown box menu to the top right corner.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Flex, Box } from 'grid-styled';
@@ -29,7 +30,15 @@ const FullNameBox = styled(Box)`
   letter-spacing: 0;
   text-align: center;
   line-height: 22px;
-  cursor: pointer;
+  user-select: none;
+`;
+
+const AngleDownIcon = styled(Box)`
+  width: 11px;
+  height: 6px;
+  margin: 3px 0 0 10px;
+  background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAJCAYAAAA7KqwyAAAABGdBTUEAALGPC/xhBQAAAMhJREFUKBVjbFr36r+XAQ+DsSInAyng7P3vDNsufGFgctDiYZiy8z3DlSc/idYPUgvSA9LL8B8IDt349j9h+tP/lx//AHHxApAakFqQHhAAGwBi7Lr05X/SjKf/bz//CeJiBSA5kBqQWhiAGwASwGcINs0gPSgGgATWn/n0P3XWk/8P3/wCccEAxAaJgeTQAYYBIAUghWmzn4INAWkGsbFpBqllBBHYgn/ZsY8MR258BUvZaHAzRFnxY1PGwIJVFCiIrAGZja4eAH/2S1Gw0p89AAAAAElFTkSuQmCC');
+  background-size: 11px 6px;
 `;
 
 const Logo = styled.img`
@@ -37,12 +46,84 @@ const Logo = styled.img`
   height: 45px;
 `;
 
-const UserDropDownBox = ({ fullName = 'Loading', email = '', onClick }) => (
-  <Flex alignItems="center" onClick={onClick}>
-    <Avatar email={email} />
-    <FullNameBox>{fullName}</FullNameBox>
-  </Flex>
-);
+const DropdownFlex = styled(Flex)`
+  position: relative;
+  cursor: pointer;
+  user-select: none;
+`;
+
+const DropdownList = styled.ul`
+  position: absolute;
+  right: 0;
+  top: 100%;
+  margin: 7px 0 0;
+  padding: .5em 0;
+  width: 175px;
+  list-style: none;
+  font-size: 14px;
+  color: #666;
+  background: #fff;
+  border: 1px solid #eee;
+`;
+
+const DropdownItem = styled.li`
+  padding: .4em 1.4em;
+  cursor: pointer;
+  &:hover {
+    background-color: #f7f7f7;
+  }
+`;
+
+class UserDropDownBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDropdownVisible: false
+    };
+  }
+
+  componentDidMount() {
+    document.body.addEventListener('click', this.handleOuterClick);
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener('click', this.handleOuterClick);
+  }
+
+  render() {
+    const { fullName = 'Loading', email = '', onClick } = this.props;
+    const { isDropdownVisible } = this.state;
+
+    return (
+      <DropdownFlex alignItems="center" onClick={this.handleClick}>
+        <Avatar email={email} />
+        <FullNameBox>Welcome, {email}</FullNameBox>
+        <AngleDownIcon />
+        {isDropdownVisible && (
+          <DropdownList>
+            <DropdownItem>My Profile</DropdownItem>
+            <DropdownItem>Change Password</DropdownItem>
+            <DropdownItem onClick={onClick}>Log Out</DropdownItem>
+          </DropdownList>
+        )}
+      </DropdownFlex>
+    );
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      isDropdownVisible: !prevState.isDropdownVisible
+    }));
+  }
+
+  handleOuterClick = ({ target }) => {
+    const ownEl = findDOMNode(this);
+    if (ownEl === target || ownEl.contains(target)) {
+      return;
+    }
+    this.setState({ isDropdownVisible: false });
+  }
+}
 
 const Header = ({
   className,
