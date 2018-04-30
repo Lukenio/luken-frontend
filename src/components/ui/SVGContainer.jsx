2393e0abf614e33e3fee901d1b32e6c2c123ea47
Ruslan Aletdinov <cgruslan@gmail.com>
2018-05-01 00:38:02 +0500

    Added icons to pages (Loans and Accounts pages). (#26)

--- src/components/ui/SVGContainer.jsx
+++ src/components/ui/SVGContainer.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Box } from 'grid-styled';
 import { cssUnit } from '../cssUtils';
 
@@ -14,6 +14,12 @@ const SVGContainer = styled(Box)`
     width: 100%;
     height: 100%;
   }
+
+  ${props => props.fill && css`
+    > svg path {
+      fill: ${props.fill};
+    }
+  `}
 `;
 
 export default SVGContainer;
