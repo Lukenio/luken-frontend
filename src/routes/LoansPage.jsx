d60b9980a6723fb57544e0bcd2ec30297c1bb5c2
Ruslan Aletdinov <cgruslan@gmail.com>
2018-05-12 21:28:24 +0500

    Added How It Works to Loans Page.

--- src/routes/LoansPage.jsx
+++ src/routes/LoansPage.jsx
@@ -63,6 +63,64 @@ const CoinsPending = styled.p`
   margin: 0;
 `;
 
+const HowWrapper = styled(Flex)`
+  padding: 20px 30px 0;
+`;
+
+const HowTitle = AccountName.extend`
+  margin-left: 0;
+  margin-bottom: 3px;
+  text-align: center;
+  font-size: 29px;
+`;
+
+const HowUndertitle = styled.p`
+  color: #808184;
+  font-size: 14px;
+  text-align: center;
+`;
+
+const HowList = styled(Flex)`
+  margin: 26px 0 28px;
+`;
+
+const HowListItem = styled(Box)`
+  text-align: center;
+  font-size: 14px;
+  color: #808184;
+  position: relative;
+`;
+
+const HowListRect = styled.div`
+  display: none;
+  position: absolute;
+  top: 34px;
+  right: ${props => props.last ? '50%' : 0};
+  left: ${props => props.first ? '50%' : 0};
+  height: 7px;
+  background-color: #d7d7d7;
+
+  @media (min-width: 1080px) {
+    display: block;
+  }
+`;
+
+const HowListNumber = styled.span`
+  position: relative;
+  display: inline-block;
+  width: 76px;
+  height: 76px;
+  color: white;
+  line-height: 1.8em;
+  text-align: center;
+  font-size: 42px;
+  font-weight: 600;
+  background-color: #d7d7d7;
+  border-radius: 50%;
+  user-select: none;
+  z-index: 1;
+`;
+
 class LoansPage extends Component {
   componentDidMount() {
     this.fetchLoans(this.props);
@@ -105,6 +163,44 @@ class LoansPage extends Component {
                   <AccountName>Crypto Loans</AccountName>
                 </Flex>
                 <Divider width={1} />
+                <HowWrapper flexDirection="column" alignItems="center">
+                  <HowTitle>How it Works</HowTitle>
+                  <HowUndertitle>
+                    Loanz.io Application Process Made Easy. Receive and Approval
+                    in as little as 12 hours.
+                  </HowUndertitle>
+                  <HowList flexWrap='wrap' justifyContent='center'>
+                    <HowListItem width={[1, 1/2, 1/2, 1/5]} pt='0' px='10px' pb={[3, 3, 3, 0]}>
+                      <HowListNumber>1</HowListNumber>
+                      <p>Submit Application and receive commitment with terms and conditions within 12 - 48 hours.</p>
+                      <HowListRect first />
+                    </HowListItem>
+                    <HowListItem width={[1, 1/2, 1/2, 1/5]} pt='0' px='10px' pb={[3, 3, 3, 0]}>
+                      <HowListNumber>2</HowListNumber>
+                      <p>Sign E-Agreement and after all paperwork completed Loan Becomes Instantly Available.</p>
+                      <HowListRect />
+                    </HowListItem>
+                    <HowListItem width={[1, 1/2, 1/2, 1/5]} pt='0' px='10px' pb={[3, 3, 3, 0]}>
+                      <HowListNumber>3</HowListNumber>
+                      <p>Transfer crypto to our secured wallet and receive fiat funds directly into your bank account.</p>
+                      <HowListRect />
+                    </HowListItem>
+                    <HowListItem width={[1, 1/2, 1/2, 1/5]} pt='0' px='10px' pb={[3, 3, 3, 0]}>
+                      <HowListNumber>4</HowListNumber>
+                      <p>Support your day-to-day personal or business operations with the cash you need.</p>
+                      <HowListRect />
+                    </HowListItem>
+                    <HowListItem width={[1, 1/2, 1/2, 1/5]} pt='0' px='10px' pb={[3, 3, 3, 0]}>
+                      <HowListNumber>5</HowListNumber>
+                      <p>At the end of the loan term, pay back the principle and receive crypto back to your wallet.</p>
+                      <HowListRect last />
+                    </HowListItem>
+                  </HowList>
+                  <AccountButton onClick={this.handleModalOpen}>
+                    Start Your Loan Application
+                  </AccountButton>
+                  <Divider width={1} mt={44} /> 
+                </HowWrapper>
                 <Flex width={1} py={20} px={30} justifyContent="space-between">
                   <Flex>
                     {accounts.map(a => (
